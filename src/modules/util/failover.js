2c3c333e641a30b6dae45e27f8be8429b82ce7d7
lixinliang <lixinliang@yy.com>
2016-09-15 23:15:26 +0800

    update
    rename

--- src/modules/util/failover.js
+++ src/modules/util/failover.js
@@ -1,10 +1,10 @@
-exports.compatible = function ( microApp ) {
+exports.failover = function ( microApp ) {
     [
         'hash',
         'filter',
     ].forEach(( methodName ) => {
         microApp[methodName] = function () {
-            console.warn(`micro-app: "${ methodName }" is an empty function.`);
+            warn(`"${ methodName }" is invalid in this browser.`);
             return this;
         };
     });
