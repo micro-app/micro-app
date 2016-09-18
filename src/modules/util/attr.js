ae96591ce108bd4059e6f481b21fe1a6cda64840
lixinliang <lixinliang@yy.com>
2016-09-18 14:17:53 +0800

    update
    add some comment

--- src/modules/util/attr.js
+++ src/modules/util/attr.js
@@ -1,5 +1,11 @@
 const { setAttribute, removeAttribute } = Element.prototype;
 
+/**
+ * [Set or remove an attribute]
+ * @param  {[String]} attribute [attribute name]
+ * @param  {[String]} value     [attribute value]
+ * @return {[Element]}          [element]
+ */
 exports.attr = function ( attribute, value ) {
     if (value === null) {
         this::removeAttribute(attribute);
