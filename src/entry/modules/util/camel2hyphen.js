ca1b2102eb34f6fda57cbdeda2fce45be0bbdc48
lixinliang <lixinliang@yy.com>
2016-10-12 13:30:42 +0800

    update
    rename some module

--- src/entry/modules/util/camel2hyphen.js
+++ src/entry/modules/util/camel2hyphen.js
@@ -1,3 +1,8 @@
+/**
+ * [Camel case to hyphen case]
+ * @param  {[String]} string [camel case]
+ * @return {[String]}        [hyphen case]
+ */
 exports.camel2hyphen = function ( string ) {
     return string.replace(/[A-Z]/g, function ( match ) {
         return '-' + match.toLowerCase();
