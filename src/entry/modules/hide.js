a066c98e57f35b9cc845bb815f22ecaed7790c4a
lixinliang <lixinliang@yy.com>
2016-10-25 09:37:42 +0800

    update
    hide

--- src/entry/modules/hide.js
+++ src/entry/modules/hide.js
@@ -1,8 +1,8 @@
 import { head } from './variable.js';
 
 /**
- * [description]
- * @param  {Element} elem
+ * Hide the elemment
+ * @param {Element} elem
  */
 export default function ( elem ) {
     head.removeChild(elem);
