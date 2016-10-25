31b57743deef5781217af187a6f348d99a29def6
lixinliang <lixinliang@yy.com>
2016-10-25 09:37:55 +0800

    update
    set attribute

--- src/entry/modules/set-attribute.js
+++ src/entry/modules/set-attribute.js
@@ -1,10 +1,10 @@
 const { setAttribute, removeAttribute } = Element.prototype;
 
 /**
- * [description]
- * @param  {Element} elem
- * @param  {String} attribute
- * @param  {AnyType} value
+ * Set or remove an attribute
+ * @param {Element} elem
+ * @param {String} attribute
+ * @param {AnyType} value
  */
 export default function ( elem, attribute, value ) {
     if (value === null) {
