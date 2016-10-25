0e7cbf1b14b48195a8086a23e8ecaee3606c2eb2
lixinliang <lixinliang@yy.com>
2016-10-25 09:37:48 +0800

    update
    show

--- src/entry/modules/show.js
+++ src/entry/modules/show.js
@@ -1,8 +1,8 @@
 import { head } from './variable.js';
 
 /**
- * [description]
- * @param  {Element} elem
+ * Show the elemment
+ * @param {Element} elem
  */
 export default function ( elem ) {
     head.appendChild(elem);
