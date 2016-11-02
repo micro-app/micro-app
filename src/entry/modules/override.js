d5301ef605caa213ef2ad2c6b6bb45f5d13561ab
李昕亮 <lixinliang@yy.com>
2016-11-02 09:14:10 +0800

    update
    override

--- src/entry/modules/override.js
+++ src/entry/modules/override.js
@@ -1,29 +1,39 @@
-import {
-    // Define static property
-    define,
-} from './util';
+import microApp from './namespace.js';
+import defineProperty from './define-property.js';
 
-exports.override = function ( name, handler ) {
-    let method = this[name];
-    if (typeof method == 'function') {
-        // `function` will be overrided
-        this::define(
-            name,
-            function () {
-                // `bubbles` as a flag
-                let bubbles = true;
-                let result = this::handler(
-                    {
-                        stopPropagation () {
-                            bubbles = false;
+/**
+ * Override a function on microApp
+ * @param  {String} name function name
+ * @param  {Function} handler function body
+ */
+export default function ( name, handler ) {
+    let method = microApp[name];
+    // Reset the method name
+    defineProperty(
+        microApp,
+        name,
+        defineProperty(
+            defineProperty(
+                function () {
+                    // `bubbles` as a flag
+                    let bubbles = true;
+                    let result = handler.call(
+                        this,
+                        {
+                            stopPropagation () {
+                                bubbles = false;
+                            },
                         },
-                    },
-                    arguments
-                );
-                // Interrupt the function chain by `event.stopPropagation` and give the `result` as return value
-                return bubbles ? method.apply(this, arguments) : result;
-            }
-        );
-    }
-    return this;
+                        arguments
+                    );
+                    // Interrupt the function chain by `event.stopPropagation` and give the `result` as return value
+                    return bubbles ? method.apply(this, arguments) : result;
+                },
+                'name',
+                name
+            ),
+            'toString',
+            function toString () { return `function ${ name }() { [native code] }` }
+        )
+    );
 };
