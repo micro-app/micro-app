ce405bc9be1e79ed39450037c484b56c395de28f
李昕亮 <lixinliang@yy.com>
2016-11-10 10:43:59 +0800

    remove
    del not a func

--- src/entry/modules/util/not-a-function.js
+++
@@ -1,7 +0,0 @@
-/**
- * [Return its value until it is not a function]
- * @param {[AnyType]} value [value]
- */
-exports.NaF = function ( value ) {
-    return typeof value == 'function' ? NaF(value()) : value;
-};
