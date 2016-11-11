f8fc311691fed9e21c39c24aef62dd0e7738c3c8
李昕亮 <lixinliang@yy.com>
2016-11-11 11:22:54 +0800

    update
    namespace

--- src/entry/modules/namespace.js
+++ src/entry/modules/namespace.js
@@ -1,4 +1,7 @@
+import {
+    doc,
+} from './variable.js';
 import append from './append.js';
 import createElement from './create-element.js';
 
-export default document.querySelector('script[micro-app]') || append(createElement('<script micro-app>'));
+export default doc.querySelector('script[micro-app]') || append(createElement('<script micro-app>'));
