8f6f57f17e4a51a31dd2ffd39e1bbdc981d333e1
李昕亮 <lixinliang@yy.com>
2016-11-14 09:46:24 +0800

    udpate
    create multi element

--- src/entry/modules/create-multi-element.js
+++ src/entry/modules/create-multi-element.js
@@ -1,56 +1,73 @@
-import {
-    // Return its value until it is not a function
-    NaF,
-    // Set or remove an attribute
-    attr,
-    // Remove an element
-    hide,
-    // Append an element in head
-    show,
-    // Create an element from html code
-    create,
-} from './util';
+import append from './append.js';
+import filter from './filter.js';
+import remove from './remove.js';
+import microApp from './namespace.js';
+import setAttribute from './set-attribute.js';
+import createElement from './create-element.js';
 
-const { getAttribute } = Element.prototype;
+const defaultArrayValue = ({}).toString.call([]);
+const defaultBase64Value = '[object Base64]';
 
-exports.createMultiElement = function ( currentItems, previousItems, config ) {
-    // A container to saved the result
-    let result = [];
-    let attributeName = config.attribute;
+/**
+ * Create multiple elements
+ * @param  {String} code html code
+ * @param  {String} attributeName an default attribute name
+ * @return {Function} func onChange of `defineAttribute`
+ */
+export default function ( code, attributeName ) {
+    // Previous items
+    let previousItems = [];
 
-    previousItems.forEach(( element ) => {
-        // Clear the element
-        element::hide();
-    });
+    // onChange
+    return function ( name, value, previous ) {
+        // Format to `Array`
+        let currentItems = (value instanceof Array) ? value.slice(0) : value === null ? [] : [value];
 
-    let length = currentItems.length;
-    for (let i = 0; i < length; i++) {
-        let value = NaF(currentItems[i]);
-        if (value === null) {
-            // Skip
-        } else {
-            // Create element and show
-            let element = createElement(config.code)::show();
-            if (typeof value == 'object') {
-                // `attributeName` should be `undefined` defaults
-                element::setAttribute(attributeName, void 0);
-                for (let key in value) {
-                    element::setAttribute(key, NaF(value[key]));
+        // Clear old elements
+        previousItems.forEach(( element ) => {
+            remove(element);
+        });
+        previousItems = [];
+
+        // Create new elements
+        let length = currentItems.length;
+        for (let i = 0; i < length; i++) {
+            let value = currentItems[i];
+            if (value !== null) {
+                // Create element
+                let element = createElement(code);
+                if (value instanceof Object) {
+                    // `attributeName` should be `undefined` defaults
+                    setAttribute(element, attributeName, void 0);
+                    for (let key in value) {
+                        setAttribute(element, key, value[key]);
+                    }
+                } else {
+                    setAttribute(element, attributeName, value);
                 }
-            } else {
-                element::setAttribute(attributeName, value);
+                // Safari can not parse the img url includes '#'
+                let attributeValue = element.getAttribute(attributeName);
+                if (attributeValue.indexOf('#') > -1) {
+                    let temp = attributeValue.split('#');
+                    setAttribute(element, attributeName, temp[0]);
+                    setAttribute(element, 'filter', temp[1]);
+                    let interrupt = false;
+                    temp[1].split('|').forEach(( filterName ) => {
+                        if (interrupt) {
+                            return;
+                        }
+                        let filterMethod = filter[filterName];
+                        if (filterMethod) {
+                            interrupt = filterMethod.call(element) === false;
+                        }
+                    });
+                }
+                // Save the element
+                previousItems.push(append(element));
             }
-            // Safari can not parse the img url which is include '#'
-            let originValue = element::getAttribute(attributeName);
-            element::setAttribute('original-' + attributeName, originValue);
-            if (originValue.indexOf('#') > -1) {
-                element::setAttribute(attributeName, originValue.split('#')[0]);
-            }
-            // Invoke `success` when element create successfully
-            config.success && config.success.call(element, originValue);
-            // Save the element
-            result.push(element);
         }
-    }
-    return result;
+
+        // Set value as attribute
+        setAttribute(microApp, name, value instanceof Array ? defaultArrayValue : /^data:image/.test(value) ? defaultBase64Value : value);
+    };
 };
