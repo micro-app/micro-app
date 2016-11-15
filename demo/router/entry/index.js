912431c4c7358acac7ce667ce1867caa327a7ed0
李昕亮 <lixinliang@yy.com>
2016-11-15 09:41:56 +0800

    update
    router

--- demo/router/entry/index.js
+++ demo/router/entry/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import VueTouch from 'vue-touch';
 
 if (navigator.standalone && location.hash.length) {
+    location.replace(decodeURIComponent(location.hash.substring(1)));
 } else {
     Vue.use(VueTouch)
     new Vue({
